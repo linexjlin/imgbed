9df585197793a4a1bd1eee020dccc30209fcdca4
linxj <line.xj.lin@gmail.com>
2020-10-24 23:12:52 +0800

    add ipfs api

--- src/providers/index.js
+++ src/providers/index.js
@@ -1,7 +1,9 @@
 import UpYun from './upyun'
 import Qiniu from './qiniu'
+import Ipfs from './ipfs'
 
 export default {
+  ipfs: Ipfs,
   upyun: UpYun,
   qiniu: Qiniu
 }
---
+++ src/providers/ipfs.js
@@ -0,0 +1,65 @@
+import _ from 'lodash'
+import axios from 'axios'
+
+export default class Ipfs {
+  BASE_URL = 'https://ipfs.ee/api/add'
+
+  static title = 'IPFS'
+
+  static params = [{
+    name: 'requestUrl',
+    label: '加速域名地址：',
+    component: 'input',
+    type: 'text',
+    placeholder: 'https://ipfs.ee/ipfs'
+  }]
+
+  static rules = {
+    requestUrl: [
+      { required: false, message: '请加速域名地址', trigger: 'blur' },
+      { type: 'url', message: '格式形如 https://ipfs.ee/ipfs', trigger: 'blur' }
+    ]
+  }
+
+  constructor (requestUrl) {
+    this.requestUrl = requestUrl
+  }
+
+  getEndpoint () {
+    return this.BASE_URL
+  }
+
+  successHandle (file, res) {
+    let burl = _.trimEnd(this.requestUrl, '/')
+    if (burl === '') {
+      burl = 'https://ipfs.ee/ipfs'
+    }
+    let url = burl + '/' + _.get(res, 'data.Hash') + '/' + _.get(res, 'data.Name')
+    return {
+      url,
+      name: file.name,
+      thumb: url + '#80x80',
+      preview: url + '#320x180'
+    }
+  }
+
+  errorHandle (err) {
+    return {
+      message: _.get(err, 'response.data.message') || err.message
+    }
+  }
+
+  upload (file, onProgress, onError, onSuccess) {
+    let url = this.getEndpoint()
+    let formData = new FormData()
+    formData.append('file', file)
+
+    axios.post(url, formData, {
+      onUploadProgress: onProgress
+    }).then((res) => {
+      return onSuccess(this.successHandle(file, res))
+    }).catch((err) => {
+      return onError(this.errorHandle(err))
+    })
+  }
+}
